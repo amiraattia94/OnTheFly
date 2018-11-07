139b1d3c6a2a52d5d56c23ea413100b2b91015f5
Altah3r <34957816+altah3r@users.noreply.github.com>
2018-11-07 19:25:25 +0200

    taher branch edit first

--- OnTheFlyWPFC/View/BranchAddUC.xaml.cs
+++ OnTheFlyWPFC/View/BranchAddUC.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnTheFlyWPFC.Model.DTO;
+using OnTheFlyWPFC.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,27 @@ namespace OnTheFlyWPFC.View
     /// </summary>
     public partial class BranchAddUC : UserControl
     {
+        CityViewModel _cityViewModel;
         public BranchAddUC()
         {
             InitializeComponent();
+            _cityViewModel = new CityViewModel();
+
+        }
+
+        private void cmbBranchCities_Loaded(object sender, RoutedEventArgs e) {
+            _cityViewModel.GetAllCities();
+            //cmbBranchCities.DataContext = _cityViewModel;
+            cmbBranchCities.ItemsSource = _cityViewModel.CityName;
+            cmbBranchCities.DisplayMemberPath = "name";
+
+
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) {
+            var a = (CityDTO)cmbBranchCities.SelectedValue;
+            MessageBox.Show("hi " + a.cityCode);
         }
     }
 }
---
+++ OnTheFlyWPFC/ViewModel/CityViewModel.cs
@@ -0,0 +1,67 @@
+﻿using OnTheFlyWPFC.Model.DTO;
+using OnTheFlyWPFC.Model.Service;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnTheFlyWPFC.ViewModel {
+    public class CityViewModel : INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName) {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private CityService _cityService;
+        public List<string> CN { get; private set; }
+
+        private ObservableCollection<CityDTO> _cityName;
+        public ObservableCollection<CityDTO> CityName {
+            get {
+                return _cityName;
+            }
+
+            set {
+                _cityName = value;
+            }
+        }
+
+        private CityDTO _selectedCity;
+        public CityDTO SelectedCity {
+            get {
+                return _selectedCity;
+            }
+
+            set {
+                _selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        
+
+        public CityViewModel() {
+            _cityName = new ObservableCollection<CityDTO>();
+            _cityService = new CityService();
+
+
+        }
+        
+        async public void GetAllCities() {
+            var cities = await _cityService.GetCity();
+            CN = new List<string>();
+            foreach(var city in cities) {
+                _cityName.Add(city);
+                CN.Add(city.name);
+                
+
+            }
+        }       
+    }
+}
