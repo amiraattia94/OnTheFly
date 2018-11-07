0e142de634f4b758492966f33e6fc7e163967ed7
amiraattia94 <amira.j.attia@gmail.com>
2018-11-07 19:57:22 +0200

    updated to taher branch

---
+++ OnTheFlyWPFC/Model/DTO/CityDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnTheFlyWPFC.Model.DTO {
+    public class CityDTO {
+        public string cityCode { get; set; }
+        public string name { get; set; }
+    }
+}
