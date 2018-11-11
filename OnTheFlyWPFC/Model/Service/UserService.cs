309d090751e63c35f79a3f07dd812c1a0abd0699
amiraattia94 <amira.j.attia@gmail.com>
2018-11-11 22:19:09 +0200

    Added Login page

---
+++ OnTheFlyWPFC/Model/Service/UserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnTheFlyWPFC.Model.DTO;
+
+namespace OnTheFlyWPFC.Model.Service
+{
+    class UserService
+    {
+        async public Task<List<UserDTO>> GetUser()
+        {
+            await Task.FromResult(true);
+
+            using (OnTheFlyDBEntities con = new OnTheFlyDBEntities())
+            {
+                List<UserDTO> result = con.userTBLs.Select(s => new UserDTO()
+                {
+                    username = s.user_name,
+                    password = s.password
+                }).ToList();
+
+                return result;
+            }
+        }
+    }
+}
---
+++ OnTheFlyWPFC/ViewModel/LoginViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using OnTheFlyWPFC.Model.DTO;
+using OnTheFlyWPFC.Model.Service;
+
+namespace OnTheFlyWPFC.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private UserService _userService;
+        public List<string> UN { get; private set; }
+
+        private ObservableCollection<UserDTO> _userName;
+        public ObservableCollection<UserDTO> UserName
+        {
+            get
+            {
+                return _userName;
+            }
+
+            set
+            {
+                _userName = value;
+            }
+        }
+
+        private UserDTO _selectedUser;
+        public UserDTO SelectedUser
+        {
+            get
+            {
+                return _selectedUser;
+            }
+
+            set
+            {
+                _selectedUser = value;
+                OnPropertyChanged("SelectedUser");
+            }
+        }
+
+
+
+        public LoginViewModel()
+        {
+            _userName = new ObservableCollection<UserDTO>();
+            _userService = new UserService();
+
+
+        }
+
+        async public void GetAllCities()
+        {
+            var users = await _userService.GetUser();
+            UN = new List<string>();
+            foreach (var user in users)
+            {
+                _userName.Add(user);
+                UN.Add(user.username);
+            }
+        }
+    }
+}
